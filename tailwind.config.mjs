4c5234bf14c7e052a9b988b97199905af5250b26
alexandru-posirca <axelltech.dev@gmail.com>
2024-03-17 23:59:53 +0200

    First setup color prop

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,22 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "1025px",
+			xl: "1200px",
+		},
+		colors: {
+			"black-main": "#000000",
+			"red": "#b70000",
+			"white-main": "#FAFAF9",
+			"green": "#38af48",
+			gray: {
+				200: "#b9b9ba",
+				500: "#171619"
+			}
+		},
 		extend: {},
 	},
 	plugins: [],
