96457343c1b119662bdcbfae0bc160f410f17556
alexandru-posirca <axelltech.dev@gmail.com>
2024-03-17 23:54:22 +0200

    Init project with tailwind css / nested css

---
+++ postcss.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    plugins:  [
+      require('postcss-import'),
+      require('postcss-nested'),
+      require('tailwindcss/nesting')(require('postcss-nesting')),
+      require('autoprefixer'),
+      require('tailwindcss'),
+    ]
+  }
