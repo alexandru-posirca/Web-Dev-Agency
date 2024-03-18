8283d99831c6ca8cd0cc4897f54d7017343f172a
alexandru-posirca <axelltech.dev@gmail.com>
2024-03-18 23:57:55 +0200

    Add local font / variables for work

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,8 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
+	mode: 'jit',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		screens: {
@@ -7,16 +10,25 @@ export default {
 			md: "768px",
 			lg: "1025px",
 			xl: "1200px",
+			"md-media": { raw: "(min-width: 768px)" },
+			"lg-media": { raw: "(min-width: 1025px)" },
+			"xl-media": { raw: "(min-width: 1200px)" },
 		},
 		colors: {
 			"black-main": "#000000",
-			"red": "#b70000",
 			"white-main": "#FAFAF9",
 			"green": "#38af48",
 			gray: {
 				200: "#b9b9ba",
 				500: "#171619"
+			},
+			red: {
+				200: "#b70000",
+				500: "#6c001a"
 			}
+		},
+		fontFamily: {
+			montserrat: ["Montserrat", ...defaultTheme.fontFamily.sans]
 		},
 		extend: {},
 	},
