b0eb771a6d7320fddc3801764d05368368634423
alexandru-posirca <axelltech.dev@gmail.com>
2024-03-19 03:53:42 +0200

    Stylization homepage content for all devices

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,35 +2,57 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	mode: 'jit',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		screens: {
-			sm: "480px",
-			md: "768px",
-			lg: "1025px",
-			xl: "1200px",
-			"md-media": { raw: "(min-width: 768px)" },
-			"lg-media": { raw: "(min-width: 1025px)" },
-			"xl-media": { raw: "(min-width: 1200px)" },
-		},
-		colors: {
-			"black-main": "#000000",
-			"white-main": "#FAFAF9",
-			"green": "#38af48",
-			gray: {
-				200: "#b9b9ba",
-				500: "#171619"
-			},
-			red: {
-				200: "#b70000",
-				500: "#6c001a"
-			}
-		},
-		fontFamily: {
-			montserrat: ["Montserrat", ...defaultTheme.fontFamily.sans]
-		},
-		extend: {},
-	},
-	plugins: [],
-}
+  mode: "jit",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1025px",
+      xl: "1200px",
+      "md-media": { raw: "(min-width: 768px)" },
+      "lg-media": { raw: "(min-width: 1025px)" },
+      "xl-media": { raw: "(min-width: 1200px)" },
+    },
+    colors: {
+      "black-main": "#000000",
+      "white-main": "#FAFAF9",
+      green: "#38af48",
+      gray: {
+        200: "#b9b9ba",
+        500: "#171619",
+      },
+      orange: {
+        200: "#F9A602",
+        500: "#FF7417",
+      },
+    },
+    fontFamily: {
+      montserrat: ["Montserrat", ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      keyframes: {
+        text_reveal_box: {
+          "50%": {
+            width: "100%",
+            left: "0",
+          },
+          "100%": {
+            width: "0",
+            left: "100%",
+          },
+        },
+        text_reveal_main: {
+          "100%": {
+            color: "#FAFAF9",
+          },
+        },
+      },
+      animation: {
+        textrevealbox: "text_reveal_box 1s ease",
+        textrevealmain: "text_reveal_main 3s ease forwards",
+      },
+    },
+  },
+  plugins: [],
+};
